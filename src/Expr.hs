dcb18f3f455b3a9e03465d906f8d4e405464a76e
Egor Gorbachev <peltorator@yandex.ru>
2020-04-08 17:42:58 +0300

    add old files

--- src/Expr.hs
+++ src/Expr.hs
@@ -1,21 +1,19 @@
 module Expr where
 
-import           AST         (AST (..), Operator (..), Subst (..))
-import           Combinators (Parser (..), Result (..), bind', elem', fail',
-                              fmap', satisfy, some', success)
-import           Data.Char   (digitToInt, isDigit)
-import qualified Data.Map as Map
+import           AST         (AST (..), Operator (..))
+import           Combinators (Parser (..), Result (..), elem', elems, fail',
+                              satisfy, success, symbol, symbols)
+import           Data.Char   (digitToInt, isDigit, isLetter)
+import Control.Applicative
 
 data Associativity
   = LeftAssoc  -- 1 @ 2 @ 3 @ 4 = (((1 @ 2) @ 3) @ 4)
   | RightAssoc -- 1 @ 2 @ 3 @ 4 = (1 @ (2 @ (3 @ 4))
   | NoAssoc    -- Может быть только между двумя операндами: 1 @ 2 -- oк; 1 @ 2 @ 3 -- не ок
 
+-- Универсальный парсер выражений
 data OpType = Binary Associativity
             | Unary
-
-evalExpr :: Subst -> AST -> Maybe Int
-evalExpr = error "evalExpr undefined"
 
 uberExpr :: Monoid e
          => [(Parser e i op, OpType)] -- список операций с их арностью и, в случае бинарных, ассоциативностью
@@ -23,36 +21,93 @@ uberExpr :: Monoid e
          -> (op -> ast -> ast -> ast) -- конструктор узла дерева для бинарной операции
          -> (op -> ast -> ast)        -- конструктор узла для унарной операции
          -> Parser e i ast
-uberExpr = error "uberExpr undefined"
-
+uberExpr ((pars, as):parsers) basicParser makeAST makeASTU = (case as of
+    Binary NoAssoc    -> do
+        left  <- getTail
+        prs   <- pars
+        right <- getTail
+        return (makeAST prs left right)
+      <|> getTail
+    Binary LeftAssoc  -> do
+        (beg, end) <- fmap (,) getTail <*> (many $ fmap (,) pars <*> getTail)
+        return (foldl (\le (op, ri) -> makeAST op le ri) beg end)
+    Binary RightAssoc  -> do
+        (beg, end) <- fmap (,) (many $ fmap (,) getTail <*> pars) <*> getTail
+        return (foldr (\(le, op) ri -> makeAST op le ri) end beg)
+    Unary             -> fmap makeASTU pars <*> getTail
+    ) <|> getTail
+ 
+  where getTail = uberExpr parsers basicParser makeAST makeASTU
+        
+uberExpr [] x _ _ = x
 
 -- Парсер для выражений над +, -, *, /, ^ (возведение в степень)
 -- с естественными приоритетами и ассоциативностью над натуральными числами с 0.
 -- В строке могут быть скобки
 parseExpr :: Parser String String AST
-parseExpr = error "parseExpr undefined"
+parseExpr = uberExpr [
+                        (make "||", Binary RightAssoc), (make "&&", Binary RightAssoc), (make "!", Unary),
+                        (make "==" <|> make "/=" <|> make "<=" <|> make "<" <|> make ">=" <|> make ">", Binary NoAssoc),
+                        (make "+" <|> make "-", Binary LeftAssoc), (make "*" <|> make "/", Binary LeftAssoc),
+                        (make "-", Unary), (make "^", Binary RightAssoc)
+                     ]
+                     (func (fmap Num parseNum) <|> func (fmap Ident parseIdent) <|> func (symbol '(' *> parseExpr <* symbol ')')) BinOp UnaryOp
+    where make c = symbols c >>= toOperator
+          func f = parseSpaces *> f <* parseSpaces
+
+
+parseNum :: Parser String String Int
+parseNum = fmap (foldl f 0) (some (satisfy isDigit))
+  where
+    f x y = 10 * x + digitToInt y
+
 
 -- Парсер для целых чисел
-parseNum :: Parser String String Int
-parseNum = foldl (\acc d -> 10 * acc + digitToInt d) 0 `fmap'` go
+parseNegNum :: Parser String String Int
+parseNegNum = fmap (foldl f 0) go
   where
+    f x '-' = -x
+    f x y   = 10 * x + digitToInt y
     go :: Parser String String String
-    go = some' (satisfy isDigit)
+    go = some (satisfy isDigit) <|> (fmap (\a b -> b ++ a) (many (symbol '-')) <*> some (satisfy isDigit))
 
 parseIdent :: Parser String String String
-parseIdent = error "parseIdent undefined"
+parseIdent = do
+    hd <- (satisfy isLetter) <|> (symbol '_')
+    tl <- many $ (satisfy isLetter) <|> (satisfy isDigit) <|> (symbol '_')
+    return (hd:tl)
 
 -- Парсер для операторов
 parseOp :: Parser String String Operator
-parseOp = elem' `bind'` toOperator
+parseOp = elems ["||", "&&", ">", ">=", "<", "<=", "/=", "==", "+", "-", "*", "/", "^", "!"]  >>= toOperator
+
+parseExactly :: String -> Parser String String String
+parseExactly = foldr (\hd tl -> fmap (:) (symbol hd) <*> tl) $ pure ""
+
+parseSpaces :: Parser String String String
+parseSpaces = many $ symbol ' ' <|> symbol '\n'
+
+parseSomeSpaces :: Parser String String String
+parseSomeSpaces = some $ symbol ' ' <|> symbol '\n'
+
 
 -- Преобразование символов операторов в операторы
-toOperator :: Char -> Parser String String Operator
-toOperator '+' = success Plus
-toOperator '*' = success Mult
-toOperator '-' = success Minus
-toOperator '/' = success Div
-toOperator _   = fail' "Failed toOperator"
+toOperator :: String -> Parser String String Operator
+toOperator "+"  = success Plus
+toOperator "*"  = success Mult
+toOperator "-"  = success Minus
+toOperator "/"  = success Div
+toOperator "^"  = success Pow
+toOperator "==" = success Equal
+toOperator "/=" = success Nequal
+toOperator "<=" = success Le
+toOperator "<"  = success Lt
+toOperator ">=" = success Ge
+toOperator ">"  = success Gt
+toOperator "&&" = success And
+toOperator "||" = success Or
+toOperator "!"  = success Not
+toOperator _    = fail' "Failed toOperator"
 
 evaluate :: String -> Maybe Int
 evaluate input = do
@@ -60,10 +115,24 @@ evaluate input = do
     Success rest ast | null rest -> return $ compute ast
     _                            -> Nothing
 
-compute :: AST -> Int
-compute (Num x)           = x
-compute (BinOp Plus x y)  = compute x + compute y
-compute (BinOp Mult x y)  = compute x * compute y
-compute (BinOp Minus x y) = compute x - compute y
-compute (BinOp Div x y)   = compute x `div` compute y
 
+hlp True = 1
+hlp False = 0
+
+compute :: AST -> Int
+compute (Num x)            = x
+compute (BinOp Plus x y)   = compute x + compute y
+compute (BinOp Mult x y)   = compute x * compute y
+compute (BinOp Minus x y)  = compute x - compute y
+compute (BinOp Div x y)    = compute x `div` compute y
+compute (BinOp Pow x y)    = (compute x) ^ (compute y)
+compute (BinOp Equal x y)  = hlp ((compute x) == (compute y))
+compute (BinOp Nequal x y) = hlp ((compute x) /= (compute y))
+compute (BinOp Le x y)     = hlp ((compute x) <= (compute y))
+compute (BinOp Lt x y)     = hlp ((compute x) < (compute y))
+compute (BinOp Ge x y)     = hlp ((compute x) >= (compute y))
+compute (BinOp Gt x y)     = hlp ((compute x) > (compute y))
+compute (BinOp And x y)    = (compute x) * (compute y)
+compute (BinOp Or x y)     = let frst = compute x in if frst == 0 then compute y else frst
+compute (UnaryOp Minus x)  = -(compute x)
+compute (UnaryOp Not x)    = if compute x == 0 then 1 else 0
--- src/LLang.hs
+++ src/LLang.hs
@@ -3,6 +3,8 @@ module LLang where
 import AST (AST (..), Operator (..), Subst (..))
 import Combinators (Parser (..))
 import qualified Data.Map as Map
+import Control.Applicative
+import Expr (parseExpr, parseNum, parseIdent, parseOp, parseExactly, parseSpaces, parseSomeSpaces)
 
 type Expr = AST
 
@@ -20,8 +22,92 @@ data LAst
   | Seq { statements :: [LAst] }
   deriving (Show, Eq)
 
-parseL :: Parser String String LAst
-parseL = error "parseL undefined"
+stmt :: LAst
+stmt =
+  Seq
+    [ Read "X"
+    , If (BinOp Gt (Ident "X") (Num 13))
+         (Write (Ident "X"))
+         (While (BinOp Lt (Ident "X") (Num 42))
+                (Seq [ Assign "X"
+                        (BinOp Mult (Ident "X") (Num 7))
+                     , Write (Ident "X")
+                     ]
+                )
+         )
+    ]
+
+type CodeParser = Parser String String LAst
+
+parseExprInBrackets :: Parser String String AST
+parseExprInBrackets = do
+    parseSpaces
+    parseExactly "("
+    parseSpaces
+    expr <- parseExpr
+    parseSpaces
+    parseExactly ")"
+    return expr
+
+parseIf :: CodeParser
+parseIf = do
+    parseExactly "if"
+    cond <- parseExprInBrackets
+    parseSpaces
+    thn <- parseSeq
+    parseSpaces
+    parseExactly "else"
+    parseSpaces
+    els <- parseSeq
+    return $ If cond thn els
+
+parseWhile :: CodeParser
+parseWhile = do
+    parseExactly "while"
+    cond <- parseExprInBrackets
+    parseSpaces
+    body <- parseSeq
+    return $ While cond body
+
+parseAssign :: CodeParser
+parseAssign = do
+    parseExactly "assign"
+    parseSomeSpaces
+    var <- parseIdent
+    expr <- parseExprInBrackets
+    return $ Assign var expr
+
+parseRead :: CodeParser
+parseRead = do
+    parseExactly "read"
+    parseSomeSpaces
+    var <- parseIdent
+    return $ Read var
+
+parseWrite :: CodeParser
+parseWrite = do
+    parseExactly "print"
+    expr <- parseExprInBrackets
+    return $ Write expr
+
+parseSeq :: CodeParser
+parseSeq = do
+    parseExactly "{"
+    parseSpaces
+    instructions <- many $ parseAnything <* parseSpaces <* parseExactly ";" <* parseSpaces
+    parseExactly "}"
+    return $ Seq instructions
+
+
+parseAnything :: CodeParser
+parseAnything = parseIf <|> parseWhile <|> parseAssign <|> parseRead <|> parseWrite <|> parseSeq
+
+parseL :: CodeParser
+parseL = do
+    parseSpaces
+    x <- parseSeq
+    parseSpaces
+    return x
 
 initialConf :: [Int] -> Configuration
 initialConf input = Conf Map.empty input []
