5fce749e842206241bb96d132e3fe7b7d77a606d
Egor Gorbachev <peltorator@yandex.ru>
2020-04-22 20:07:45 +0300

    LEval.hs done

--- src/LEval.hs
+++ src/LEval.hs
@@ -3,7 +3,9 @@ module LEval where
 import LLang (Program (..), Configuration (..))
 
 evalProg :: Program -> [Int] -> Maybe Configuration
-evalProg = error "evalProg not implemented"
+evalProg (Program functions main) inp = eval main (Conf Map.empty inp [] (Map.fromList (fmap (\func -> (func, name func))) functions))
 
 parseAndEvalProg :: String -> [Int] -> Maybe Configuration
-parseAndEvalProg = error "parseAndEvalProg not implemented"
+parseAndEvalProg code inp = case (runParser parseProg code) of
+    Success (InputStream stream curPos) result -> if (curPos == length stream) then evalProg result inp else Nothing
+    Failure _                                  -> Nothing
